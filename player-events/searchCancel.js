1a16a302828d35cab926d827b4b3abfc5ec38144
eleagnt <81573856+eleagnt@users.noreply.github.com>
2021-04-07 17:01:40 +0300

    Delete searchCancel.js

--- player-events/searchCancel.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client, message, query, tracks) => {
-
-    message.channel.send(`${client.emotes.error} - You did not provide a valid response ... Please send the command again !`);
-            console.log("🅸🆂 | ᴱᴸᴱᴳᴬᴺᵀ ᴹᴬᴺ  7")
-};
