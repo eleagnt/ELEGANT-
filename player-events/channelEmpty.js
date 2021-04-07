80f25fb620d3d620142fbf65b588224052a84fb5
eleagnt <81573856+eleagnt@users.noreply.github.com>
2021-04-07 17:00:57 +0300

    Delete channelEmpty.js

--- player-events/channelEmpty.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client, message, queue) => {
-
-    message.channel.send(`${client.emotes.error} - Music stopped as there is no more member in the voice channel !`);
-            console.log("🅸🆂 | ᴱᴸᴱᴳᴬᴺᵀ ᴹᴬᴺ   1")
-};
