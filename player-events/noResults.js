b77485f57be81edaaf65edd22cba6ad56785d37b
eleagnt <81573856+eleagnt@users.noreply.github.com>
2021-04-07 17:01:14 +0300

    Delete noResults.js

--- player-events/noResults.js
+++
@@ -1,5 +0,0 @@
-module.exports = (client, message, query) => {
-
-    message.channel.send(`${client.emotes.error} - No results found on YouTube for ${query} !`);
-            console.log("🅸🆂 | ᴱᴸᴱᴳᴬᴺᵀ ᴹᴬᴺ   4")
-};
